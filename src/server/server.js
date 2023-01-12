b1c089bedbee00bab4ca0cc0a5df05d8282542c6
qwerkpjh <qwerkpjh@naver.com>
2023-01-12 15:50:32 +0900

    2023-01-12 DB연동

--- src/server/server.js
+++ src/server/server.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const app = express();
-const port = 3001;
+const port = 3001; //3000 과 다른 포트
 const cors = require("cors");
 const bodyparse = require("body-parser");
 const mysql = require("mysql");
 
 
 var connection = mysql.createConnection({
-    host : "localhost",
-    user : "root",
-    password : "root",
-    database : "sql_study",
+    host : "localhost", //호스트이름 
+    user : "root", //유저 
+    password : "root", //패스워드
+    database : "sql_study", // DB명 
     insecureAuth : true,
 });
 
-connection.connect();
-console.log(connection);
+connection.connect(); //연결 
 
 app.use(bodyparse.urlencoded({extended : false}));
 app.use(cors());
@@ -29,10 +28,10 @@ app.post("/" , (req, res) => {
     const userid = req.body.userid;
     console.log(userid);
     //query를 추가할 부분 
-    connection.query("INSERT INTO ReactTest (userid) VALUES(?)" , [userid])
+    connection.query("INSERT INTO ReactTest (userid) VALUES(?)" , [userid]) //쿼리 날려주기
 
 });
 
 app.listen(port, () => {
-    console.log("example app listening at http://localhost:${port}");
+    console.log("example app listening at http://localhost:" + {port});
 });
